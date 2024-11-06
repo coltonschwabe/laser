5d8b6bbccfe2e6973f577a820c3a46dccb2d787b
Colton Schwabe <cs720@duke.edu>
2024-11-06 13:42:27 -0500

    blackbody2

--- blackbodycode.py
+++ blackbodycode.py
@@ -48,13 +48,12 @@ redbacktotal = numpy.sum(RedOffData[:,:,0])
 bluebacktotal = numpy.sum(BlueOffData[:,:,2])
 greenbacktotal = numpy.sum(GreenOffData[:,:,1])
 
-print("Red Total:" + str(redtotal))
-print("Green Total:" + str(greentotal))
-print("Blue Total:" + str(bluetotal))
+#print("Red Total:" + str(redtotal))
+#print("Green Total:" + str(greentotal))
+#print("Blue Total:" + str(bluetotal))
 
-print("RedB Total:" + str(redbacktotal))
-print("GreenB Total:" + str(greenbacktotal))
-print("BlueB Total:" + str(bluebacktotal))
+#print("RedB Total:" + str(redbacktotal))
+#print("BlueB Total:" + str(bluebacktotal))
 
 #exposure times for [red, green, blue]
 exposure = [5000, 20000, 70000]
@@ -64,29 +63,66 @@ redI = (redtotal - redbacktotal)/exposure[0]
 greenI = (greentotal - greenbacktotal)/exposure[1]
 blueI = (bluetotal - bluebacktotal)/exposure[2]
 
-# gets frequencies
-strfreqs = []
-with open("FB450-40.txt", "rt") as cont:
-    for line in cont:
-        strfreqs.append(line)
+print(redI/greenI)
+print(blueI/redI)
+print(greenI/blueI)
 
-f = []
+#calculates temps
+temps = []
 
-#extracts frequencies from txt file
-for unit in strfreqs:
-    f.append(float(unit[3:12]) * 10**(int(unit[15])-9))
+for t in range(1000, 6000, 10):
+    temps.append(t)
 
-T = 3000
-intensity = []
+Rrg = []
+Rgb = []
+Rbr = []
 
-for l in f:
-    result = (2*(math.pi)*h*c*c/(l**5))/(math.exp(h*c/(l*k*T)) - 1)
+redBas = 0.93389338
+greenBas = 0.83253431
+blueBas = 0.58647080
 
-    intensity.append(result)
+redFilt = 0.69452750
+greenFilt = 0.67064220
+blueFilt = 0.71580720
+
+for temp in temps:
+    rw = 650 / (10**9)
+    gw = 550 / (10**9)
+    bw = 450 / (10**9)
+
+    rg = ((redFilt)*(redBas)*(gw**5)*(math.exp(h*c/(gw*k*temp)) - 1))/((greenFilt)*(greenBas)*(rw**5)*(math.exp(h*c/(rw*k*temp)) - 1))
+    gb = ((greenFilt)*(greenBas)*(bw**5)*(math.exp(h*c/(bw*k*temp)) - 1))/((blueFilt)*(blueBas)*(gw**5)*(math.exp(h*c/(gw*k*temp)) - 1))
+    br = ((blueFilt)*(blueBas)*(rw**5)*(math.exp(h*c/(rw*k*temp)) - 1))/((redFilt)*(redBas)*(bw**5)*(math.exp(h*c/(bw*k*temp)) - 1))
+
+    Rrg.append(rg)
+    Rgb.append(gb)
+    Rbr.append(br)
 
 plt.figure(1)
-plt.plot(f,intensity,'-r')
+plt.plot(temps,Rrg,'-r')
+plt.plot(temps, Rbr, '-b')
+plt.plot(temps, Rgb, '-g')
 plt.show()
+
+# gets frequencies
+#strfreqs = []
+#with open("FB450-40.txt", "rt") as cont:
+#    for line in cont:
+ #       strfreqs.append(line)
+
+#f = []
+
+#extracts frequencies from txt file
+#for unit in strfreqs:
+#    f.append(float(unit[3:12]) * 10**(int(unit[15])-9))
+
+#T = 3000
+#intensity = []
+
+#for l in f:
+#    result = (2*(math.pi)*h*c*c/(l**5))/(math.exp(h*c/(l*k*T)) - 1)
+
+#    intensity.append(result)
 
 #newim=Image.fromarray(Data)
 #newim.show()
